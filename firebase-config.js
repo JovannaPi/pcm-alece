3b7b0e0bdeb25ff32d577d44aa2a527a032aeefc
JovannaPi <jovannapinheiro@alu.ufc.br>
2026-07-27 07:59:42 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { getAuth, signInAnonymously } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDlXUNfjDlmvOWxlgVRgJeyMQ2ntD7qxJg",
@@ -12,3 +13,9 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+export const auth = getAuth(app);
+
+// Faz o login ANTES do app.js começar a rodar. Sem esta espera, os
+// listeners do Firestore disparariam sem usuário autenticado e tomariam
+// "permission denied" na hora que a regra passar a exigir login.
+await signInAnonymously(auth);
